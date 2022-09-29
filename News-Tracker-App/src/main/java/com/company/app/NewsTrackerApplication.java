3f9b7921f553b7bf3300749b005ffc7444a1f125
Alok Gupta <alokik@zohomail.in>
2022-09-29 19:05:49 +0530

    fixed

--- News-Tracker-App/src/main/java/com/company/app/NewsTrackerApplication.java
+++ News-Tracker-App/src/main/java/com/company/app/NewsTrackerApplication.java
@@ -1,14 +1,22 @@
 package com.company.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class NewsTrackerApplication implements WebMvcConfigurer{
+public class NewsTrackerApplication implements WebMvcConfigurer {
+
+	private static final Logger logger = LoggerFactory.getLogger(NewsTrackerApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewsTrackerApplication.class, args);
-		
+
+		logger.info("this is a info message");
+		logger.warn("this is a warn message");
+		logger.error("this is a error message");
+
 	}
 }
